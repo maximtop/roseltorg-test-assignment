081f99d9190d664afe3c8a56a7a0c3a2c6ded6e4
Topciu Maxim <maximtop@gmail.com>
2017-10-23 22:33:25 +0300

    add generators

--- promises.js
+++ promises.js
@@ -2,16 +2,35 @@ import Promise from 'bluebird';
 
 Promise.promisifyAll(api);
 
-const getDataOrCreateUser = async () => {
+// const getDataOrCreateUser = async () => {
+//   try {
+//     const userInfo = await getUserCurrentAsync();
+//     if (userInfo) {
+//       const organizationInfo = await getOrganiaztionInfoAsync(userInfo.organization_id);
+//       if (userInfo.scopes.indexOf('orders') !== -1) {
+//         const orders = await getOrdersAsync();
+//       }
+//     } else {
+//       await createUser(data);
+//       getDataOrCreateUser();
+//     }
+//   }
+//   catch (e) {
+//     console.log('Не в силах')
+//   }
+// }
+
+
+function* getDataOrCreateUser () {
   try {
-    const userInfo = await getUserCurrentAsync();
+    const userInfo = yield getUserCurrentAsync();
     if (userInfo) {
-      const organization = await getOrganiaztionInfoAsync(userInfo.organization_id);
+      const organizationInfo = yield getOrganiaztionInfoAsync(userInfo.organization_id);
       if (userInfo.scopes.indexOf('orders') !== -1) {
-        const orders = await getOrdersAsync();
+        const orders = yield getOrdersAsync();
       }
     } else {
-      await createUser(data);
+      yield createUser(data);
       getDataOrCreateUser();
     }
   }
